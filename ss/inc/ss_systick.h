a225d6f6ae80cb42d1fbe71090ca5075a248a003
Oszi <oszimilian@oszimilian.net>
2025-04-16 20:18:54 +0200

    some ajustments on systick-handle

--- ss/inc/ss_systick.h
+++ ss/inc/ss_systick.h
@@ -1,6 +1,8 @@
 #ifndef _SS_SYSTICK_H_
 #define _SS_SYSTICK_H_
 
+#define MAX_SYSTICK_HANDLE_COUNT 10
+
 
 typedef struct Systick_Handle
 {
@@ -9,6 +11,17 @@ typedef struct Systick_Handle
     volatile uint32_t tick;
 
 } Systick_Handle;
+
+struct SystickCntr {
+    Systick_Handle handler[MAX_SYSTICK_HANDLE_COUNT];
+    uint8_t count;
+};
+
+void ss_init_systick_cntr(struct SystickCntr* cntr);
+
+uint8_t ss_systick_add_handle(struct SystickCntr* cntr, uint32_t period);
+
+bool ss_systick_expired(struct SystickCntr* cntr, uint8_t id);
 
 void sys_tick_handler(void);
 
--- ss/src/ss_systick.c
+++ ss/src/ss_systick.c
@@ -8,12 +8,19 @@ extern Systick_Handle handle_pid;
 extern Systick_Handle handle_error;
 extern Systick_Handle handle_tod;
 
+extern struct SystickCntr systick_cntr;
+
 void sys_tick_handler(void) {
+    for (uint8_t i = 0; i < systick_cntr.count; i++) {
+        systick_cntr.handler[i].tick++;
+    }
+    /*
     handle1.tick++;
     handle2.tick++;
     handle_pid.tick++;
     handle_error.tick++;
     handle_tod.tick++;
+    */
 }
 
 int8_t ss_init_systick(uint32_t reload) {
@@ -30,4 +37,20 @@ bool ss_handle_timer(Systick_Handle* handle) {
     if (handle->timer > handle->tick) return false;
     handle->timer = (handle->tick - handle->timer) > handle->period ? handle->tick + handle->period : handle->timer + handle->period;
     return true;
+}
+
+void ss_init_systick_cntr(struct SystickCntr* cntr) {
+    cntr->count = 0;
+}
+
+uint8_t ss_systick_add_handle(struct SystickCntr* cntr, uint32_t period) {
+    cntr->handler[cntr->count].timer = 0;
+    cntr->handler[cntr->count].period = period;
+    cntr->handler[cntr->count].tick = 0;
+    
+    return cntr->count++;
+}
+
+bool ss_systick_expired(struct SystickCntr* cntr, uint8_t id) {
+    return ss_handle_timer(&cntr->handler[id]);
 }
