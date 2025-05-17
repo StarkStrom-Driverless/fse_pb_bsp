50a965d1a4c44ade26f8a8b9ba4b67b140556ce2
Maximilian Hoffmann <maximilian.hoffmann@starkstrom-augsburg.de>
2025-05-17 22:50:00 +0200

    add init stuff

--- test/main.c
+++ test/main.c
@@ -10,6 +10,28 @@
 #include "ss_iob.h"
 #include "ss_spi.h"
 
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+#ifdef SS_USE_RTOS
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+extern void vApplicationStackOverflowHook( TaskHandle_t xTask, char * pcTaskName );
+
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char * pcTaskName) {
+	for(;;);	// Loop forever here..
+}
+
+static void task1(void *args __attribute((unused))) {
+
+	for (;;) {
+		ss_led_heartbeat_toggle(); 
+		vTaskDelay(pdMS_TO_TICKS(500));
+	}
+}
+#endif
 
 
 #define REAR 0
@@ -24,22 +46,23 @@ struct IOB iob;
 
 
 
+
+
+
+
+
+
+
  
 int main(void)
 {
     /**************************************************************************
      *                                      SYSTICK                           *     
      *************************************************************************/
-    /**
-     * SYSTICK INIT
-     */
-    ss_init_systick(160);
 
-    /**
-     * SYSTICK HANDLE
-     */
-    ss_init_systick_cntr(&systick_cntr);
-    uint8_t heartbeat_handle = ss_systick_add_handle(&systick_cntr, 2000);
+
+    uint16_t pin = ss_io_init(PIN('A', 15), SS_GPIO_MODE_OUTPUT);
+    ss_io_write(pin, SS_GPIO_OFF);
 
 
 
@@ -53,21 +76,27 @@ int main(void)
 
 
 
-
+    #ifdef SS_USE_RTOS
+	xTaskCreate(task1,"LED",100,NULL,configMAX_PRIORITIES-1,NULL);
+	vTaskStartScheduler();
+    #endif
 
     while (1) {
 
         
         
-
+        //ss_led_heartbeat_toggle();
         
 
         /**
          * heartbeat
          */
+
+        /*
         if (ss_systick_expired(&systick_cntr, heartbeat_handle)) {
             ss_led_heartbeat_toggle(); 
         }
+        */
 
 
 	}
